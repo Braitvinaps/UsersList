7e375bd1b1ca189aeec964a385912421889a63b8
Alexey Britvin <britvinaps@gmail.com>
2022-11-11 15:48:34 +0300

    pagination

--- users-table/src/components/app/App.js
+++ users-table/src/components/app/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Modal from '../modal/modal';
+import Pagination from '../pagination/pagination';
 import SearchPanel from '../search-panel/search-panel';
 import UsersList from '../users-list/users-list';
 import './App.css';
@@ -11,40 +12,53 @@ function App() {
   const [users, setUsers] = useState([]);
   const [modal, setModal] = useState(false);
 
+  const [currentPage, setCurrentPage] = useState(1)
+  const [dataPerPage] = useState(5)
+
   useEffect(() => {
     const getUsers = async () => {
       const res = await axios.get(URL)
       setUsers(res.data)
     }
     getUsers()
-  },[])
+  }, [])
 
+  // пагинация
+  const lastDataIndex = currentPage * dataPerPage
+  const firstDataIndex = lastDataIndex - dataPerPage
+  const currentData = users.slice(firstDataIndex, lastDataIndex)
+  const paginate = pageNumber => setCurrentPage(pageNumber)
 
-return (
-  <div className="App">
-    <div className="wrapper">
-      <div className="container">
-        <h1>Список пользователей</h1>
-        <SearchPanel />
-        <UsersList
-          users={users}
-          setUsers={setUsers}
-          setModal={setModal} />
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="container">
+          <h1>Список пользователей</h1>
+          <SearchPanel />
+          <UsersList
+            users={currentData}
+            setUsers={setUsers}
+            setModal={setModal} />
+        </div>
       </div>
+      <Modal
+        title={'Вы уверены, что хотите удалить пользователя?'}
+        isOpened={modal}
+      >
+        <div className="btn-modal">
+          <button className='btn modal-yes'>Да</button>
+          <button
+            className='btn modal-no'
+            onClick={() => setModal(false)}>Нет</button>
+        </div>
+      </Modal>
+      <Pagination
+        currentPage={currentPage}
+        dataPerPage={dataPerPage}
+        totalData={users.length}
+        paginate={paginate} />
     </div>
-    <Modal
-      title={'Вы уверены, что хотите удалить пользователя?'}
-      isOpened={modal}
-    >
-      <div className="btn-modal">
-        <button className='btn modal-yes'>Да</button>
-        <button
-          className='btn modal-no'
-          onClick={() => setModal(false)}>Нет</button>
-      </div>
-    </Modal>
-  </div>
-);
+  );
 }
 
 export default App;
