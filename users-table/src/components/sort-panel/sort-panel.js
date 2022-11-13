0f8a90eb9c1d98f02ab86c28716e068ad177617c
Alexey Britvin <britvinaps@gmail.com>
2022-11-13 19:31:54 +0300

    sortBtn

--- users-table/src/components/sort-panel/sort-panel.js
+++ users-table/src/components/sort-panel/sort-panel.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './sort-panel.css'
 
 function SortPanel() {
+    const[regData, setRegData] = useState(false);
+    const[rating, setRating] = useState(false);
+    
+    let classNames = (btn) => {
+        return `btn sort-btn ${btn ? 'active' : null}`
+    }
+
     return (
         <div className="sort-panel">
             <label>Сортировка: </label>
-            <button className="btn sort-btn">Дата регистрации</button>
-            <button className="btn sort-btn">Рейтинг</button>
+            <button 
+                onClick={() => {
+                    setRegData(true)
+                    setRating(false)
+                }}
+                className={classNames(regData)}>Дата регистрации</button>
+            <button 
+                onClick={() => {
+                    setRating(true)
+                    setRegData(false)
+                }}
+                className={classNames(rating)}>Рейтинг</button>
         </div>
     )
 }
